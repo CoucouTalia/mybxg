fc84bc2503b6dcbb757f0c71c5e01b1cc4b7559b
zhang <1234567@qq.com>
2017-09-21 16:37:13 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,10 +8,40 @@ define(['jquery','template'],function($,template){
         dataType:'json',
         url:'/api/teacher',
         success:function(data){
+            //console.log(data)
          var html=template('teacherTpl',{list:data.result});
             $("#teacherInfo").html(html);
+            //启用和注销功能
+            $(".eod").click(function(){
+               //console.log(111)
+                var that=this;
+                var td = $(this).closest('td');
+                var tcId=td.attr('data-tcId');
+                var tcStatus=td.attr('data-status');
+                //console.log(tcStatus);
+                //调后台接口
+                $.ajax({
+                    type:'post',
+                    url:'/api/teacher/handle',
+                    data:{
+                        tc_id:tcId,
+                        tc_status:tcStatus
+                    },
+                    dataType:'json',
+                    success:function(data){
+                       console.log(data)
+                        if(data.code==200){
+                            td.attr('data-status',data.result.tc_status);
+                            if(data.result.tc_status==1){
+                                $(that).text('启用');
+                            }else{
+                                $(that).text('注销');
+                            }
+                        }
 
+                    }
+                });
+            });
         }
-
-    })
-})
+    });
+});
