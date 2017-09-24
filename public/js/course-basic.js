ecc15abed016a7bb6ce56092aa2d69977f3294c9
zhang <1234567@qq.com>
2017-09-24 22:34:43 +0800

    完成课程基本信息填充

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -6,7 +6,6 @@ define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR
     util.setMenu('/course/add');
   //获取课程id
     var csId=util.qs('cs_id');
-    console.log(csId)
     //操作标志位
     var flag = util.qs('flag');
     //根据id查询课程详细信息
@@ -25,6 +24,28 @@ define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR
             //解析数据，渲染页面
             var html=template('basicTpl',data.result);
             $('#basicInfo').html(html);
+            //处理二级分类的下拉联动操作
+            $('#firstType').change(function(){
+               //获取一级分类的id
+                var pid=$(this).val();
+                //根据一级分类的id查询所有的二级分类数据
+                $.ajax({
+                    type:'get',
+                    url:'/api/category/child',
+                    data:{cg_id:pid},
+                    dataType:'json',
+                    success:function(data){
+                       //console.log(data)
+                        var tpl='<option value="">请选择二级分类...</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                       var html= template.render(tpl,{list:data.result});
+                        console.log(html)
+                        $('#secondType').html(html);
+
+                    }
+                })
+
+            })
+            
         }
     })
 })
