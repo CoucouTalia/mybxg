cf3b39f9cb93af1d9340816a2f2907b3f70dcf0a
zhang <1234567@qq.com>
2017-09-25 17:06:52 +0800

    完成课程基本信息更新功能

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,7 +1,7 @@
 /**
  * Created by admin on 2017/9/24.
  */
-define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR){
+define(['jquery','template','util','ckeditor','validate','form'],function($,template,util,CKEDITOR){
 //设置导航菜单选中
     util.setMenu('/course/add');
   //获取课程id
@@ -14,38 +14,50 @@ define(['jquery','template','util','ckeditor'],function($,template,util,CKEDITOR
         url:'/api/course/basic',
         data:{cs_id:csId},
         dataType:'json',
-        success:function(data){
-           console.log(data)
-            if(flag){
-                data.result.operate='编辑课程';
-            }else{
-                data.result.operate='添加课程';
+        success:function(data) {
+            console.log(data)
+            if (flag) {
+                data.result.operate = '编辑课程';
+            } else {
+                data.result.operate = '添加课程';
             }
             //解析数据，渲染页面
-            var html=template('basicTpl',data.result);
+            var html = template('basicTpl', data.result);
             $('#basicInfo').html(html);
             //处理二级分类的下拉联动操作
-            $('#firstType').change(function(){
-               //获取一级分类的id
-                var pid=$(this).val();
+            $('#firstType').change(function () {
+                //获取一级分类的id
+                var pid = $(this).val();
                 //根据一级分类的id查询所有的二级分类数据
                 $.ajax({
-                    type:'get',
-                    url:'/api/category/child',
-                    data:{cg_id:pid},
-                    dataType:'json',
-                    success:function(data){
-                       //console.log(data)
-                        var tpl='<option value="">请选择二级分类...</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
-                       var html= template.render(tpl,{list:data.result});
+                    type: 'get',
+                    url: '/api/category/child',
+                    data: {cg_id: pid},
+                    dataType: 'json',
+                    success: function (data) {
+                        //console.log(data)
+                        var tpl = '<option value="">请选择二级分类...</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                        var html = template.render(tpl, {list: data.result});
                         console.log(html)
                         $('#secondType').html(html);
-
                     }
-                })
-
-            })
-            
+                });
+            });
+            //处理表单提交
+            $('#basicForm').validate({
+                sendForm: false,
+                valid: function () {
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/course/update/basic',
+                        data: {cs_id: csId},
+                        dataType: 'json',
+                        success: function (data) {
+                            location.href='/course/picture?cs_id='+data.result.cs_id;
+                        }
+                    });
+                }
+            });
         }
-    })
-})
+    });
+});
