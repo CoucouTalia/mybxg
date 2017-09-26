1221e421e124cca763fc17369d5ab28044532ce5
zhang <1234567@qq.com>
2017-09-26 11:57:10 +0800

    完成修改剪切图片的方法

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -90,17 +90,18 @@ define(['jquery','template','util','uploadify','jcrop','form'],function($,templa
                  //创建一个选区
                  this.newSelection();
                  this.setSelect([x,y,w,h]);
+                 //监控选区的变化
+                 img.parent().on('cropstart cropmove cropend',function(a,b,c){
+                     console.log(c)
+                     //选区完成和变化的时候把对应的坐标数据填充到表单里
+                     var aInput=$('#cropForm').find('input');
+                     aInput.eq(0).val(c.x);
+                     aInput.eq(1).val(c.y);
+                     aInput.eq(2).val(c.w);
+                     aInput.eq(3).val(c.h);
+                 });
              });
-             //监控选区的变化
-             img.parent().on('cropstart cropmove cropend',function(a,b,c){
-                 console.log(c)
-                 //选区完成和变化的时候把对应的坐标数据填充到表单里
-                var aInput=$('#cropForm').find('input');
-                 aInput.eq(0).val(c.x);
-                 aInput.eq(1).val(c.y);
-                 aInput.eq(2).val(c.w);
-                 aInput.eq(3).val(c.h);
-             });
+
          }
         }
     });
