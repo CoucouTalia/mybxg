9246aa2beb0a34e5b8024c32a5d04413c32693c0
zhang <1234567@qq.com>
2017-09-27 20:56:56 +0800

    最后完成

---
+++ public/js/state.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 2017/9/27.
+ */
+define(['jquery'],function($){
+   $(document).ajaxStart(function(){
+       //显示遮罩层
+     $('.overlay').show();
+   });
+    $(document).ajaxStop(function(){
+        //隐藏遮罩层
+        setTimeout(function(){
+             $('.overlay').hide();
+        },500)
+
+    });
+});
